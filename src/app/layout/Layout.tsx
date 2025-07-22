5b853c8e941a65bdd3c67f191f7a84ee4e7afcfb
makasinui <mks.shi@ya.ru>
2025-07-22 15:55:34 +0300

    feat(layout): add outlet for children

--- src/app/layout/Layout.tsx
+++ src/app/layout/Layout.tsx
@@ -1,9 +1,13 @@
 import { Sidebar } from "@/shared"
+import { Outlet } from "react-router"
 
 export default function Layout() {
     return (
         <main className="layout">
             <Sidebar />
+            <section className="layout-children">
+                <Outlet />
+            </section>
         </main>
     )
 }
