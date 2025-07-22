0ebb19ad1269b72412f2ab4827a1ffaef54a09d0
makasinui <mks.shi@ya.ru>
2025-07-22 16:30:44 +0300

    fix(app): corrent store path

--- src/app/providers/AppProvider.tsx
+++ src/app/providers/AppProvider.tsx
@@ -1,6 +1,6 @@
 import type { ReactNode } from 'react';
 import { Provider } from 'react-redux';
-import { store } from '../store';
+import { store } from '../store/store';
 
 interface AppProviderProps {
     children: ReactNode;
