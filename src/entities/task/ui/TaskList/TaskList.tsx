e4ed2ef1b31a6ba0267d1f6080cc71364c664cbc
makasinui <mks.shi@ya.ru>
2025-07-26 23:37:10 +0300

    feat(tasks): create kanban view

--- src/entities/task/ui/TaskList/TaskList.tsx
+++ src/entities/task/ui/TaskList/TaskList.tsx
@@ -1,26 +1,77 @@
 import styles from './TaskList.module.scss';
-import type { Task } from '../../model/types';
+import { TaskStatus, type Task } from '../../model/types';
 import { TaskCard } from '../TaskCard';
+import { firstCharToUpperCase } from '@/shared/lib';
 
 interface TaskListProps {
     tasks?: Task[];
+    view?: 'list' | 'todo';
     onDelete?: (id: string) => void;
     onEdit?: (task: Task) => void;
 }
 
-export default function TaskList({ tasks, onDelete, onEdit }: TaskListProps) {
+const TaskWrapperList = ({ tasks, onDelete, onEdit }: TaskListProps) => {
+    return (
+        <div className={styles['task__list-wrapper']}>
+            {tasks?.map((task) => (
+                <TaskCard
+                    key={task.id}
+                    task={task}
+                    onDelete={onDelete}
+                    onEdit={onEdit}
+                />
+            ))}
+        </div>
+    );
+};
+
+const TaskWrapperToDo = ({ tasks, onDelete, onEdit }: TaskListProps) => {
+    const getSortedTasks = (status: TaskStatus) => {
+        return tasks?.filter((task) => task.status === status);
+    };
+
+    const sortedColumn = Object.values(TaskStatus).sort((a, b) => a.localeCompare(b));
+
+    return (
+        <div className={styles['task__list-kanban']}>
+            {sortedColumn.map((status, i) => (
+                <div
+                    className={styles['task__list-column']}
+                    key={i}
+                >
+                    {getSortedTasks(status)?.map((task) => (
+                        <div key={task.id}>
+                            <h3>{firstCharToUpperCase(status)}</h3>
+                            <TaskCard
+                                key={task.id}
+                                task={task}
+                                onDelete={onDelete}
+                                onEdit={onEdit}
+                            />
+                        </div>
+                    ))}
+                </div>
+            ))}
+        </div>
+    );
+};
+
+export default function TaskList({ tasks, view = 'todo', onDelete, onEdit }: TaskListProps) {
     return (
         <div className={styles.task__list}>
-            <div className={styles['task__list-wrapper']}>
-                {tasks?.map((task) => (
-                    <TaskCard
-                        key={task.id}
-                        task={task}
-                        onDelete={onDelete}
-                        onEdit={onEdit}
-                    />
-                ))}
-            </div>
+            {view === 'list' ? (
+                <TaskWrapperList
+                    tasks={tasks}
+                    onDelete={onDelete}
+                    onEdit={onEdit}
+                />
+            ) : (
+                <TaskWrapperToDo
+                    tasks={tasks}
+                    onDelete={onDelete}
+                    onEdit={onEdit}
+                />
+            )}
         </div>
     );
 }
