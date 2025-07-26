481830ac4b11d997630bf374610cf33e14e4bf4c
makasinui <mks.shi@ya.ru>
2025-07-27 00:57:57 +0300

    feat(ui): create input component

--- src/shared/ui/index.ts
+++ src/shared/ui/index.ts
@@ -2,5 +2,6 @@ import { Badge } from './badge';
 import Sidebar from './sidebar';
 import { Button } from './button';
 import { Modal } from './modal';
+import Input from './input';
 
-export { Sidebar, Badge, Button, Modal };
+export { Sidebar, Badge, Button, Modal, Input };
---
+++ src/shared/ui/input/Input.tsx
@@ -0,0 +1,74 @@
+import type { ZodSchema } from 'zod/v3';
+import styles from './Input.module.scss';
+import { useEffect, useState, type ChangeEvent } from 'react';
+import z, { ZodError } from 'zod';
+
+interface InputProps {
+    value: string;
+    label?: string;
+    validationSchema?: ZodSchema;
+    onValidationError?: (error: string[] | null) => void;
+    onChange: (e: string) => void;
+}
+
+export default function Input({ value, label, validationSchema, onValidationError, onChange }: InputProps) {
+    const [isFocused, setIsFocused] = useState(false);
+    const [error, setError] = useState<string[] | null>(null);
+
+    const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+        onChange(e.target.value);
+    };
+
+    const shouldFloat = isFocused || !!value;
+
+    const handleFocus = () => {
+        setIsFocused(true);
+    };
+
+    const handleBlur = () => {
+        setIsFocused(false);
+    };
+
+    useEffect(() => {
+        if (validationSchema) {
+            try {
+                validationSchema.parse(value);
+                setError(null);
+                onValidationError?.(null);
+            } catch (err) {
+                const { errors } = z.treeifyError(err as ZodError);
+                if (errors?.length) {
+                    setError(errors);
+                    onValidationError?.(errors);
+                }
+            }
+        }
+    }, [value, validationSchema, onValidationError]);
+
+    return (
+        <div className={styles['input-wrapper']}>
+            <div className={styles['input-wrapper__content']}>
+                <input
+                    className={styles['input-field']}
+                    value={value}
+                    onChange={handleChange}
+                    onFocus={handleFocus}
+                    onBlur={handleBlur}
+                />
+                {label && <span className={`${styles['input-label']} ${shouldFloat ? styles['input-label--floating'] : ''}`}>{label}</span>}
+            </div>
+            {error?.length ? (
+                <div className={styles['input-error__wrapper']}>
+                    {error?.map((err) => (
+                        <span
+                            key={err}
+                            className={styles['input-error']}
+                        >
+                            {err}
+                        </span>
+                    ))}
+                </div>
+            ) : null}
+        </div>
+    );
+}
